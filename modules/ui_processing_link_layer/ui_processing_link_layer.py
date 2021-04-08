f723a97a42f37a307001070f603b7ea2725a50e3
ntflix <felix@iron59.co.uk>
2021-04-08 14:10:03 +0100

    Update TODO

--- modules/ui_processing_link_layer/ui_processing_link_layer.py
+++ modules/ui_processing_link_layer/ui_processing_link_layer.py
@@ -34,4 +34,5 @@ if __name__ == "__main__":
 # | • 'Save' function            | #
 # | • 'Load' function            | #
 # | • onMazeSolved method        | #
+# | • 'Clear' funct. (see reqs)  | #
 # |——————————————————————————————| #
